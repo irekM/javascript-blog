082574d8c3878a7a0848528a58840c03e3588707
IrekM <12935670+irekM@users.noreply.github.com>
2024-10-02 01:06:22 +0200

    add 'active' class to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
+
+  //[DONE]remove class active from all article links
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
 }
 
-//remove class active from all article links
+//[IN PRPOGRESS]add class active to the clicked link
 
-//add class active to the clicked link
+console.log('clickedElement:', clickedElement);
+clickedElement.classList.add('active');
 
 //remove class active from all articles
+
+const activeArticles = document.querySelectorAll('.post-content');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
 //get 'href' attribute from the clicked link
 
@@ -22,3 +37,5 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+}
+
